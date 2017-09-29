34dfd38b9f7f03ea96c0b4eadfefd0b90305f0ca
wzwmzm <wzw_mzm@aliyun.com>
2017-09-29 13:47:57 +0800

    0

---
+++ src/qtexample/2.qml/rcc_cgo_android_linux_arm.go
@@ -0,0 +1,8 @@
+// +build android
+
+package main
+
+/*
+#cgo LDFLAGS: -Wl,--allow-shlib-undefined
+*/
+import "C"
---
+++ src/qtexample/spide/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/henrylee2cn/pholcus/exec"
+
+	_ "lib"
+)
+
+func main() {
+
+	exec.DefaultRun("web")
+
+}
