36ae95937a259d3a0c9cc03aa2c7a5a3b6b5e9dd
wzwmzm <wzw_mzm@aliyun.com>
2017-10-08 20:50:28 +0800

    1

--- src/qtexample/4.audio/main.go
+++ src/qtexample/4.audio/main.go
@@ -12,7 +12,7 @@ import (
 func main() {
 	var app = widgets.NewQApplication(len(os.Args), os.Args)
 
-	var w = NewWidget(nil, 0)
+	var w = NewWidget(nil, 0) //NewWidget()是moc.go文件自动生成的,是Widget数据结构的接口函数(类Widget的成员函数),这里是初始化了Widget数据结构
 	w.Show()
 
 	app.Exec()
