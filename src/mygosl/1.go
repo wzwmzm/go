5087b4fc8d2641518eea3a259faebb46130d79a1
wzwmzm <wzw_mzm@aliyun.com>
2018-06-08 18:32:51 +0800

    a

---
+++ src/mygosl/1.go
@@ -0,0 +1,10 @@
+package mylib
+
+import (
+	"fmt"
+)
+
+func SayHello() bool {
+	fmt.Printf("Hello World from mylib 吴志伟!\n")
+	return true
+}
---
+++ src/mygosl/t_1_test.go
@@ -0,0 +1,12 @@
+package mylib
+
+import (
+	"testing"
+)
+
+func TestMylib(tst *testing.T) {
+	res := SayHello()
+	if !res {
+		tst.Errorf("test failed\n")
+	}
+}
