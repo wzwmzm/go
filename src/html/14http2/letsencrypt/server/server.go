055b344158d847aae471cf5c1dea8b8d3297372a
wzwmzm <wzw_mzm@aliyun.com>
2018-10-04 22:54:42 +0800

    a

--- src/html/14http2/letsencrypt/server/server.go
+++ src/html/14http2/letsencrypt/server/server.go
@@ -8,16 +8,21 @@ import (
 	"golang.org/x/crypto/acme/autocert"
 )
 
+//报错:
+//“listen tcp :443: bind: permission denied”
+//处理:
+//在linux中,缺省状态下应用程序不能监听小号端口,必须先给应用程序提权后才行
+//sudo setcap CAP_NET_BIND_SERVICE=+eip ./server
+
 func main() {
 	certManager := autocert.Manager{
 		Prompt:     autocert.AcceptTOS,
-		HostPolicy: autocert.HostWhitelist("localhost"), //("example.com"),
+		HostPolicy: autocert.HostWhitelist("lsg.wzw.com"), //("example.com"),
 		Cache:      autocert.DirCache("certs"),
 	}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello world"))
 	})
-
 	server := &http.Server{
 		Addr: ":443",
 		TLSConfig: &tls.Config{
@@ -26,5 +31,4 @@ func main() {
 	}
 	go http.ListenAndServe(":80", certManager.HTTPHandler(nil))
 	log.Fatal(server.ListenAndServeTLS("", "")) //Key and cert are coming from Let's Encrypt
-
 }
