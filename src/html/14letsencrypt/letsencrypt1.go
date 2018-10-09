e337b0964dcd94fed8c3f2303f1eb4fb71e9c684
wzwmzm <wzw_mzm@aliyun.com>
2018-10-09 20:21:11 +0800

    a

---
+++ src/html/14letsencrypt/letsencrypt1.go
@@ -0,0 +1,32 @@
+// Package main provide one-line integration with letsencrypt.org
+package main
+
+import (
+	"fmt"
+
+	"github.com/kataras/iris"
+)
+
+//sudo setcap CAP_NET_BIND_SERVICE=+eip ./letsencrypt1
+
+func main() {
+	app := iris.New()
+
+	app.Get("/", func(ctx iris.Context) {
+		ctx.Writef("Hello from SECURE SERVER!")
+	})
+
+	app.Get("/test2", func(ctx iris.Context) {
+		ctx.Writef("Welcome to secure server from /test2!")
+	})
+
+	app.Get("/redirect", func(ctx iris.Context) {
+		ctx.Redirect("/test2")
+	})
+
+	// NOTE: This will not work on domains like this,
+	// use real whitelisted domain(or domains split by whitespaces)
+	// and a non-public e-mail instead.
+	fmt.Println("http://gofans.ga...")
+	app.Run(iris.AutoTLS("gofans.ga:443", "gofans.ga www.gofans.ga", "wzwmzm41@gmail.com"))
+}
---
+++ src/html/14letsencrypt/letsencrypt2.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"crypto/tls"
+	"log"
+	"net/http"
+
+	"golang.org/x/crypto/acme/autocert"
+)
+
+func main() {
+	certManager := autocert.Manager{
+		Prompt:     autocert.AcceptTOS,
+		HostPolicy: autocert.HostWhitelist("gofans.ga www.gofans.ga"),
+		Cache:      autocert.DirCache("certs"),
+	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello world"))
+	})
+	server := &http.Server{
+		Addr: ":443",
+		TLSConfig: &tls.Config{
+			GetCertificate: certManager.GetCertificate,
+		},
+	}
+	go http.ListenAndServe(":80", certManager.HTTPHandler(nil))
+	log.Fatal(server.ListenAndServeTLS("", "")) //Key and cert are coming from Let's Encrypt
+}
