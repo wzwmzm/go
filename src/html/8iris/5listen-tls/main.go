16951fccc53014b02a6d71accee03a5304fb9f40
wzwmzm <wzw_mzm@aliyun.com>
2018-10-21 21:50:51 +0800

    a

--- src/html/8iris/5listen-tls/main.go
+++ src/html/8iris/5listen-tls/main.go
@@ -22,10 +22,10 @@ func main() {
 
 	// to start a new server listening at :80 and redirects
 	// to the secure address, then:
-	target, _ := url.Parse(":443")                   ////
-	go host.NewProxy(":80", target).ListenAndServe() ////
+	target, _ := url.Parse("https://127.0.0.1:443")                   ////
+	go host.NewProxy("127.0.0.1:80", target).ListenAndServe() ////
 
 	// start the server (HTTPS) on port 443, this is a blocking func
-	app.Run(iris.TLS(":443", "mycert.cer", "mykey.key")) ////
+	app.Run(iris.TLS("127.0.0.1:443", "mycert.cer", "mykey.key")) ////
 
 }
