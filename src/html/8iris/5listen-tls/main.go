5afc0b2267512b14c51118f97254dd5d7f4574fb
wzwmzm <wzw_mzm@aliyun.com>
2018-10-29 21:04:25 +0800

    a

--- src/html/8iris/5listen-tls/main.go
+++ src/html/8iris/5listen-tls/main.go
@@ -26,6 +26,6 @@ func main() {
 	go host.NewProxy("gofans.ga:80", target).ListenAndServe() ////
 
 	// start the server (HTTPS) on port 443, this is a blocking func
-	app.Run(iris.TLS("gofans.ga:443", "mycert.cer", "mykey.key")) ////
+	app.Run(iris.TLS("192.168.2.2:443", "mycert.cer", "mykey.key")) ////
 
 }
