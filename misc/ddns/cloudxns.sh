c94c30e7a331f384ea696aec09ead0ee175973ce
wzwmzm <wzw_mzm@aliyun.com>
2018-11-19 18:07:29 +0800

    a

--- misc/ddns/cloudxns.sh
+++ misc/ddns/cloudxns.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo "==========================================================begin====$(date)" 
+#echo "==========================================================begin====$(date)" 
 CONFIG=$1
 
 if [ ! -f "$CONFIG" ];then
@@ -17,11 +17,14 @@ fi
 
 IP=""
 RETRY="0"
+IDSTR="(ID=$(date +%H:%M:%S))"
 while [ $RETRY -lt 3 ]; do
     IP=$(curl -s ip.xdty.org)
     RETRY=$((RETRY+1))
-    if [ -z "$IP" ];then
-        echo "RETRY $RETRY TIME..."
+    echo "$IP"|grep "^[0-9]\{1,3\}\.\([0-9]\{1,3\}\.\)\{2\}[0-9]\{1,3\}$" > /dev/null;
+    if [ $? -ne 0 ]
+    then
+        echo "$IDSTR $(date +%H:%M:%S) RETRY $RETRY TIME..."
 	sleep 6
     else
         break
@@ -33,14 +36,14 @@ echo "$IP"|grep "^[0-9]\{1,3\}\.\([0-9]\{1,3\}\.\)\{2\}[0-9]\{1,3\}$" > /dev/nul
 if [ $? -ne 0 ]
 then
 	echo "进入IP地址合法性检测第一段++++++++++++++++++++++++++++++++++++++++++++"
-	echo "$(date) --- IP地址非法" 
+	echo "$IDSTR $(date +%H:%M:%S)  --- IP地址非法" 
         echo "前次地址: $LAST_IP   "
 	echo "本次地址: $IP"
-#	ip addr
+	ip addr
         echo "重启WIFI连接....sudo ip link set wlan0 down...."
         sudo ip link set wlan0 down
         sleep 10
-        echo "$(date) --- 重启WIFI后IP=$(curl -s ip.xdty.org) "
+        echo "$IDSTR $(date +%H:%M:%S) --- 重启WIFI后IP=$(curl -s ip.xdty.org) "
         echo ""
 	echo ""
 
@@ -68,11 +71,11 @@ if [ -z "$IP" ];then
 fi
 
 if [ "$IP" = "$LAST_IP" ];then
-    echo "当前_IP = 上次_IP : $IP = $LAST_IP   $(date) -- Already updated."
+    echo "$(date) --- Already updated.($IP)"
     exit 0
 fi
 
-echo "当前_IP = 上次_IP : $IP = $LAST_IP   $(date) -- 需要更新域名IP."
+echo "当前IP($IP)   上次IP($LAST_IP)   $(date) -- 需要更新域名IP."
 
 URL_D="https://www.cloudxns.net/api2/domain"
 DATE=$(date)
