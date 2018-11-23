1d63123150fe32dfb49b342255cd76eef65834da
wzwmzm <wzw_mzm@aliyun.com>
2018-11-23 16:44:30 +0800

    a

--- misc/ddns/cloudxns.sh
+++ misc/ddns/cloudxns.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-#echo "==========================================================begin====$(date)" 
 CONFIG=$1
 
 if [ ! -f "$CONFIG" ];then
@@ -39,17 +38,14 @@ then
 	echo "$IDSTR $(date +%H:%M:%S)  --- IP地址非法" 
         echo "前次地址: $LAST_IP   "
 	echo "本次地址: $IP"
-#	ip addr
         echo "重启WIFI连接....sudo ip link set wlan0 down...."
         sudo ip link set wlan0 down
         sleep 10
         echo "$IDSTR $(date +%H:%M:%S) --- 重启WIFI后IP=$(curl -s ip.xdty.org) "
         echo ""
 	echo ""
-
         return 1
 fi
-
 #因为有了上面一段代码,所以本段代码可以省略. 但因为不影响结果,所以本段代码暂且保留.
 if [ -z "$IP" ];then
 	echo "进入IP地址合法性检测第二段++++++++++++++++++++++++++++++++++++++++++++"
@@ -59,14 +55,6 @@ if [ -z "$IP" ];then
         sleep 10
 	echo "$(date) --- 重启WIFI后IP=$(curl -s ip.xdty.org) "
 	echo ""
-#	echo "ping -c2 gofans.ga..............................."
-#	ping -c2 gofans.ga
-##	echo "top -n 1 -b  ...................................."
-##	top -n 1 -b 
-#	echo "ping -c2 online.sh.cn............................"
-#	ping -c2 online.sh.cn
-#	echo "ping -c2 192.168.2.1............................."
-#       ping -c2 192.168.2.1
 	exit 1
 fi
 
@@ -77,21 +65,21 @@ fi
 
 echo "当前IP($IP)   上次IP($LAST_IP)   $(date) -- 需要更新域名IP."
 
+echo  "更新1: HOST=WWW  111111111111111111111111111111111111111111"
+HOST="www"
+
 URL_D="https://www.cloudxns.net/api2/domain"
 DATE=$(date)
 HMAC_D=$(printf "%s" "$API_KEY$URL_D$DATE$SECRET_KEY"|md5sum|cut -d" " -f1)
 DOMAIN_ID=$(curl -k -s $URL_D -H "API-KEY: $API_KEY" -H "API-REQUEST-DATE: $DATE" -H "API-HMAC: $HMAC_D"|grep -o "id\":\"[0-9]*\",\"domain\":\"$DOMAIN"|grep -o "[0-9]*"|head -n1)
 
 echo "DOMAIN ID: $DOMAIN_ID"
-
 URL_R="https://www.cloudxns.net/api2/record/$DOMAIN_ID?host_id=0&row_num=500"
 HMAC_R=$(printf "%s" "$API_KEY$URL_R$DATE$SECRET_KEY"|md5sum|cut -d" " -f1)
 RECORD_ID=$(curl -k -s "$URL_R" -H "API-KEY: $API_KEY" -H "API-REQUEST-DATE: $DATE" -H "API-HMAC: $HMAC_R"|grep -o "record_id\":\"[0-9]*\",\"host_id\":\"[0-9]*\",\"host\":\"$HOST\""|grep -o "record_id\":\"[0-9]*"|grep -o "[0-9]*")
 
 echo "RECORD ID: $RECORD_ID"
-
 URL_U="https://www.cloudxns.net/api2/record/$RECORD_ID"
-
 PARAM_BODY="{\"domain_id\":\"$DOMAIN_ID\",\"host\":\"$HOST\",\"value\":\"$IP\"}"
 HMAC_U=$(printf "%s" "$API_KEY$URL_U$PARAM_BODY$DATE$SECRET_KEY"|md5sum|cut -d" " -f1)
 
@@ -105,7 +93,4 @@ if [ "$(printf "%s" "$RESULT"|grep -c -o "message\":\"success\"")" = 1 ];then
 else
     echo "$(date) -- Update failed"
 fi
-
-
-
 
