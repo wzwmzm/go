a4e40f2829fb053c2e8e9309cec59278bd32be6c
wzwmzm <wzw_mzm@aliyun.com>
2018-12-11 20:33:22 +0800

    a

--- misc/rc.local.g808.sh
+++ misc/rc.local.g808.sh
@@ -1,4 +1,4 @@
-sudo hostname -b k8mp;
+sudo hostname -b g808;
 #"/home/wzw/project/go/src/html/9fileserver(net.http)/"fileserver.k8mp &
 #cd "/home/wzw/project/go/src/laboratory/"
 #./arm-laboratory &
