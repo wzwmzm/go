499227622bdc4261b167133853ca6fddd87d760d
wzwmzm <wzw_mzm@aliyun.com>
2018-12-13 13:51:12 +0800

    a

--- misc/ddns/cloudxns.a808t.sh
+++ misc/ddns/cloudxns.a808t.sh
@@ -17,7 +17,7 @@ fi
 
 IP=""
 RETRY="0"
-while [ $RETRY -lt 5 ]; do
+while [ $RETRY -lt 3 ]; do
     IP=$(curl -s ip.xdty.org)
     RETRY=$((RETRY+1))
     echo "$IP"|grep "^[0-9]\{1,3\}\.\([0-9]\{1,3\}\.\)\{2\}[0-9]\{1,3\}$" > /dev/null;
@@ -51,30 +51,9 @@ then
 	echo ""
         return 1
 fi
-#因为有了上面一段代码,所以本段代码可以省略. 但因为不影响结果,所以本段代码暂且保留.
-if [ -z "$IP" ];then
-	echo "进入IP地址合法性检测第二段++++++++++++++++++++++++++++++++++++++++++++"
-	echo "$(date) --- 无法获得外网地址.***  前次地址: $LAST_IP   ***"
-
-        echo "$(ping -c2 www.baidu.com)"
-        echo "$(ping -c2 gofans.ga)"
-        echo "----------------------ping-------------------------------"
-        ping -c2 www.baidu.com
-        ping -c2 gofans.ga
-
-	echo "重启WIFI连接....sudo ip link set wlan0 down...."
-	sudo ip link set wlan0 down
-        sleep 10
-	echo "$(date) --- 重启WIFI后IP=$(curl -s ip.xdty.org) "
-	echo ""
-	exit 1
-fi
 
 if [ "$IP" = "$LAST_IP" ];then
     echo "$IDSTR $(date +%H:%M:%S) ---$RETRY--- Already updated.($IP)"
     exit 0
 fi
 
-echo "当前IP($IP)   上次IP($LAST_IP)   $(date) -- 需要更新域名IP."
-
-echo "LAST_IP=\"$IP\"" > "$LAST_IP_FILE"
