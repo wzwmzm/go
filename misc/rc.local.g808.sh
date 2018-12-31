aba000a729290588c1d93a8a6572af60a54d56f1
wzwmzm <wzw_mzm@aliyun.com>
2018-12-31 15:38:17 +0800

    a

--- misc/rc.local.g808.sh
+++ misc/rc.local.g808.sh
@@ -3,4 +3,5 @@ sudo hostname -b g808;
 #cd "/home/wzw/project/go/src/laboratory/"
 #./arm-laboratory &
 
+#     sudo setcap CAP_NET_BIND_SERVICE=+eip /usr/local/bin/caddy
 caddy -port 44444 -conf  /home/wzw/project/go/src/html/8iris/7proxy-caddy/Caddyfile &
