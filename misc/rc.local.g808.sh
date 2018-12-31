2bd0541004e1f6d698a991296a8ef67fa88362e6
wzwmzm <wzw_mzm@aliyun.com>
2018-12-31 21:37:44 +0800

    a

--- misc/rc.local.g808.sh
+++ misc/rc.local.g808.sh
@@ -1,7 +1,17 @@
 sudo hostname -b g808;
+
 "/home/wzw/project/go/src/html/9fileserver(net.http)/"fileserver-arm &
+
 #cd "/home/wzw/project/go/src/laboratory/"
 #./arm-laboratory &
 
-#     sudo setcap CAP_NET_BIND_SERVICE=+eip /usr/local/bin/caddy
+
+#1, 在官网选择下载: https://caddyserver.com/download
+#2, 选择脚本方式安装: One-step installer script (bash):
+#        curl https://getcaddy.com | bash -s personal
+#3, 脚本执行完成后，执行 which caddy，可以看到 caddy 已被
+#        安装到了 /usr/local/bin/caddy；
+#        caddy -version 查看版本号
+#4, sudo setcap CAP_NET_BIND_SERVICE=+eip /usr/local/bin/caddy
+#5, 运行 : caddy -port 44444 -conf  /home/wzw/project/go/src/html/8iris/7proxy-caddy/Caddyfile
 caddy -port 44444 -conf  /home/wzw/project/go/src/html/8iris/7proxy-caddy/Caddyfile &
