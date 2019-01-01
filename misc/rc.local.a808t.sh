d05eda3659960ffed6b9af63ed000f8d7a45d0bb
wzwmzm <wzw_mzm@aliyun.com>
2019-01-01 19:13:06 +0800

    a

--- misc/rc.local.a808t.sh
+++ misc/rc.local.a808t.sh
@@ -1,4 +1,8 @@
 sudo hostname -b a808t;
+
 "/home/wzw/project/go/src/html/9fileserver(net.http)/"fileserver-arm &
+
 cd "/home/wzw/project/go/src/laboratory/"
 ./arm-laboratory &
+
+sudo sh -c "echo 0 >/sys/kernel/mm/ksm/run"
