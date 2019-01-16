b5f71cebae158a37e60a715573852eabe38dd795
wzwmzm <wzw_mzm@aliyun.com>
2019-01-16 19:41:22 +0800

    a

--- misc/g808-kill-app-process.sh
+++ misc/g808-kill-app-process.sh
@@ -5,5 +5,10 @@ then
 	ps -ef|grep dev|grep app|awk '{print $2}'|xargs sudo kill -9;
 	sudo rm /dev/app_process;
 else 
-	echo 没发现有app_process进程 >> /home/wzw/project/go/log/kill_app_process.log;
+#	echo 没发现有app_process进程 >> /home/wzw/project/go/log/kill_app_process.log;
+        echo 没发现有app_process进程;
+
 fi
+
+    echo "$IDSTR $(date +%H:%M:%S)  ip neigh show|wc -l = `ip neigh show|wc -l` " >> /home/wzw/project/go/log/kill_app_process.log;
+
