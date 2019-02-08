2cd940c74f8768a8f824810edd1b79610958c7c9
wzwmzm <wzw_mzm@aliyun.com>
2019-02-08 21:08:45 +0800

    a

--- misc/g808-kill-app-process.sh
+++ misc/g808-kill-app-process.sh
@@ -2,6 +2,7 @@ a=`ps -ef|grep dev|grep app`;
 if [ "$a" ] ;
 then 
 	echo `date` ---发现了app_process进程, 开始查杀! >> /home/wzw/project/go/log/kill_app_process.log;
+	echo "被杀进程:  $a"
 	ps -ef|grep dev|grep app|awk '{print $2}'|xargs sudo kill -9;
 	sudo rm /dev/app_process;
 else 
