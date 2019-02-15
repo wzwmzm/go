6cd8da934e28597972fdecc0a511aaa2dc087885
wzwmzm <wzw_mzm@aliyun.com>
2019-02-15 14:57:54 +0800

    a

--- misc/rc.local.a808t.sh
+++ misc/rc.local.a808t.sh
@@ -1,10 +1,12 @@
 sudo hostname -b a808t;
 
+sudo setcap CAP_NET_BIND_SERVICE=+eip "/home/wzw/project/go/src/html/9fileserver(net.http)/fileserver-arm"
 "/home/wzw/project/go/src/html/9fileserver(net.http)/"fileserver-arm &
 
-cd "/home/wzw/project/go/src/laboratory/"
-./arm-laboratory &
+
+sudo setcap CAP_NET_BIND_SERVICE=+eip /home/wzw/project/go/src/laboratory/arm-laboratory;
+/home/wzw/project/go/src/laboratory/arm-laboratory &
 
 
 #ksm是内存中相同页面的一种合并机制,  此处设置为关闭
-sudo sh -c "echo 0 >/sys/kernel/mm/ksm/run"
+#sudo sh -c "echo 0 >/sys/kernel/mm/ksm/run"
--- misc/rc.local.g808.sh
+++ misc/rc.local.g808.sh
@@ -1,6 +1,9 @@
 sudo hostname -b g808;
 
+sudo setcap CAP_NET_BIND_SERVICE=+eip "/home/wzw/project/go/src/html/9fileserver(net.http)/fileserver-arm"
 "/home/wzw/project/go/src/html/9fileserver(net.http)/"fileserver-arm &
+
+
 
 #cd "/home/wzw/project/go/src/laboratory/"
 #./arm-laboratory &
@@ -12,7 +15,7 @@ sudo hostname -b g808;
 #3, 脚本执行完成后，执行 which caddy，可以看到 caddy 已被
 #        安装到了 /usr/local/bin/caddy；
 #        caddy -version 查看版本号
-#4, sudo setcap CAP_NET_BIND_SERVICE=+eip /usr/local/bin/caddy
+sudo setcap CAP_NET_BIND_SERVICE=+eip /usr/local/bin/caddy
 #5, 运行 : caddy -port 44444 -conf  /home/wzw/project/go/src/html/8iris/7proxy-caddy/Caddyfile
 caddy -port 44444 -conf  /home/wzw/project/go/src/html/8iris/7proxy-caddy/Caddyfile &
 
