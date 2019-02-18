5afce96c0a99b9e7d9437b2f679eaf1e5a9f5a41
wzwmzm <wzw_mzm@aliyun.com>
2019-02-18 19:32:42 +0800

    a

--- misc/rc.local.a808t.sh
+++ misc/rc.local.a808t.sh
@@ -4,8 +4,8 @@ sudo setcap CAP_NET_BIND_SERVICE=+eip "/home/wzw/project/go/src/html/9fileserver
 "/home/wzw/project/go/src/html/9fileserver(net.http)/"fileserver-arm &
 
 
-sudo setcap CAP_NET_BIND_SERVICE=+eip /home/wzw/project/go/src/laboratory/arm-laboratory;
-/home/wzw/project/go/src/laboratory/arm-laboratory &
+#sudo setcap CAP_NET_BIND_SERVICE=+eip /home/wzw/project/go/src/laboratory/arm-laboratory;
+#/home/wzw/project/go/src/laboratory/arm-laboratory &
 
 
 #ksm是内存中相同页面的一种合并机制,  此处设置为关闭
